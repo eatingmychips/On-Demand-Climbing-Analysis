c5af1c4baf85ceaf8e1a3d1178c5750f720eab24
eatingmychips <lachlan.fitzgerald1@gmail.com>
2024-09-03 17:36:11 +1000

    Added stimulation moments in x,y,z plot and working dictionary to store points

--- pre_analysis.py
+++ pre_analysis.py
@@ -8,36 +8,36 @@ from scipy import signal
 import random
 
 def file_read(file):
-    df = pd.read_csv(file, skiprows=2)
+    df = pd.read_csv(file)
     
-    new_column_names = ['coords', 'topx', 'topy', 'likelihood', 'middlex', 'middley', 'likelihood', 'bottomx', 'bottomy', 'likelihood' ]
-    
-    df.columns = new_column_names
 
-    size = range(len(df.get('middlex').to_numpy()))
+    size = range(len(df.get('middle_x').to_numpy()))
 
     #Read in top
-    topx = df.get('topx').to_numpy()
-    topy = df.get('topy').to_numpy()
+    topx = df.get('top_x').to_numpy()
+    topy = df.get('top_y').to_numpy()
+    topz = df.get('top_z').to_numpy()
     top =[]
     for i in size: 
-        top.append([topx[i], topy[i]])
+        top.append([topx[i], topy[i], topz[i]])
 
     #Read in middle 
-    middlex = df.get('middlex').to_numpy()
-    middley = df.get('middley').to_numpy()
+    middlex = df.get('middle_x').to_numpy()
+    middley = df.get('middle_y').to_numpy()
+    middlez = df.get('middle_z').to_numpy()
     middle = []
     for i in size: 
-        middle.append([middlex[i], middley[i]])
+        middle.append([middlex[i], middley[i], middlez[i]])
 
 
 
     #Read in bottom 
-    bottomx = df.get('bottomx').to_numpy()
-    bottomy = df.get('bottomy').to_numpy()
+    bottomx = df.get('bottom_x').to_numpy()
+    bottomy = df.get('bottom_y').to_numpy()
+    bottomz = df.get('bottom_z').to_numpy()
     bottom = []
     for i in size: 
-        bottom.append([bottomx[i], bottomy[i]])
+        bottom.append([bottomx[i], bottomy[i], bottomz[i]])
 
 
     #Declare list of body parts for use in later code
@@ -59,7 +59,7 @@ def body_vel(middle):
             body_v.append(norm)
         
     body_v = pd.Series(body_v)
-    body_v = round(body_v.ewm(alpha = 0.08, adjust= False).mean(), 5)
+    body_v = round(body_v.ewm(alpha = 0.01, adjust= False).mean(), 5)
     body_v = body_v.tolist()
 
     return body_v
@@ -68,22 +68,102 @@ def moving_avg(part):
     
         partx = []
         party = []
+        partz = []
+
         for i in part: 
             partx.append(i[0])
             party.append(i[1])
+            partz.append(i[2])
         
         partx = pd.Series(partx)
         party = pd.Series(party)
-        
+        partz = pd.Series(partz)
+
         partx = round(partx.ewm(alpha=0.2, adjust= False).mean(), 100)
         partx = partx.tolist()
 
         party = round(party.ewm(alpha=0.2, adjust= False).mean(), 100)
         party = party.tolist()
 
+        partz = round(partz.ewm(alpha=0.2, adjust= False).mean(), 100)
+        partz = partz.tolist()
+
 
         smooth_data = []
         for i in range(len(partx)):
-            smooth_data.append([partx[i], party[i]])
+            smooth_data.append([partx[i], party[i], partz[i]])
 
         return smooth_data
+
+
+### Plot x and y values w.r.t time ###
+def xyzplot(parts, name):
+    points = parts[0]
+    stims = parts[1]
+    print(stims)
+    top = points[0]
+    middle = points[1]
+    bottom = points[2]
+    topx, topy, topz = zip(*top)
+    middlex, middley, middlez = zip(*middle)
+    bottomx, bottomy, bottomz = zip(*bottom)
+
+    fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+
+    # Plot x coordinates for top, middle, bottom
+    axs[0].plot(topx, label='Top', marker='o')
+    axs[0].plot(middlex, label='Middle', marker='o')
+    axs[0].plot(bottomx, label='Bottom', marker='o')
+    axs[0].set_title('X Coordinates')
+    axs[0].legend()
+
+    # Plot y coordinates for top, middle, bottom
+    axs[1].plot(topy, label='Top', marker='o')
+    axs[1].plot(middley, label='Middle', marker='o')
+    axs[1].plot(bottomy, label='Bottom', marker='o')
+    axs[1].set_title('Y Coordinates')
+    axs[1].legend()
+
+    # Plot z coordinates for top, middle, bottom
+    axs[2].plot(topz, label='Top', marker='o')
+    axs[2].plot(middlez, label='Middle', marker='o')
+    axs[2].plot(bottomz, label='Bottom', marker='o')
+    axs[2].set_title('Z Coordinates')
+    axs[2].legend()
+
+    # Set a common x-label for all subplots
+    for ax in axs:
+        for stim in stims:
+            ax.axvline(x=stim, color='r', linestyle='--', label='Stimulus' if stim == stims[0] else "")
+        ax.set_xlabel('Frames')
+        ax.legend()
+
+    axs[0].set_title(name)
+
+    # Adjust layout
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_3d(parts):
+    top = parts[0]
+    middle = parts[1]
+    bottom = parts[2]
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    topx, topy, topz = zip(*top)
+    middlex, middley, middlez = zip(*middle)
+    bottomx, bottomy, bottomz = zip(*bottom)
+    ax.scatter(topx, topy, topz)
+    ax.scatter(middlex, middley, middlez)
+    ax.scatter(bottomx, bottomy, bottomz)
+
+    plt.show()
+
+
+def get_stim_list(path): 
+    with open(path, newline='') as csvfile: 
+        reader = csv.reader(csvfile)
+        row = next(reader)
+
+    return list(map(int, row))
--- statistical_analysis.py
+++ statistical_analysis.py
@@ -10,68 +10,164 @@ from pre_analysis import *
 import statistics as stat
 import matplotlib.patches as mpatches
 from os import listdir
-
+import os
 
 ######## Here we import the files necessary for analysis, we also import the representative files for gait plotting ########
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
-    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+    return [filename for filename in filenames if filename.endswith( suffix ) ]
 
 
 
-files = [r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\Presentation\\" + x 
-         for x in find_csv_filenames(r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\Presentation")]
+files = [r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\RawData\20240719\\" + x 
+         for x in find_csv_filenames(r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\RawData\20240719")]
 
-files = [r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\Presentation\beetle_8_trial_10_camera-1_clipDLC_resnet50_TwoCamDLCAug13shuffle1_100000.csv"]
-########### End of file collection ###########
+stim_files = [r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\RawData\20240719\Stimulations\\" + x 
+         for x in find_csv_filenames(r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\RawData\20240719\Stimulations")]
+
+
+stim_folder = r"C:\Users\lachl\OneDrive\Thesis\OnDemandClimbing\RawData\20240719\Stimulations"
+
+########### End of file collection ########### 
+
 
 def stat_analysis(files): 
+    pos_dict = {}   
+    for file in files:
+        filename = os.path.splitext(os.path.basename(file))[0]
+        stim_name = filename + 'stimulation.csv'
+        stim_path = f"{stim_folder}\\{stim_name}"
+        stim = get_stim_list(stim_path)
+        parts = file_read(file)
+        top = moving_avg(parts[0])
+        middle = moving_avg(parts[1])
+        bottom = moving_avg(parts[2])
+        body_v = body_vel(middle)
         
-        for file in files:
-            parts = file_read(file)
-            top = moving_avg(parts[0])
-            middle = moving_avg(parts[1])
-            print(len(middle))
-            bottom = moving_avg(parts[2])
-            body_v = body_vel(middle)
-            
+        points = [top, middle, bottom]
+        pos_dict[filename] = [points, stim]
+
+    return body_v, pos_dict
+
+##TODO: Implement body angle compared to wall, Is the wall on the left or right side. What is the latency of climb, from first stimulation? How many stimulations until successful climb? 
+
+body_v, pos_dict = stat_analysis(files)
+
+
+
+
+
+for key, value in pos_dict.items():
+    xyzplot(value, key)
+
+
+
+
+
+
+
+
+#### Animation of Velocity ####
+
+"""
+def animation_vel(): 
+    fig, ax = plt.subplots()
+    line, = ax.plot(time, body_v)
+
+    # Set up plot limits
+    ax.set_xlim(0, len(time))
+    ax.set_ylim(0, 1.5)
+    ax.set_title("Body Velocity vs Time (Frames) (Right Elytra Stimulation)")
+    ax.set_xlabel("Frames (fps = 100)")
+    ax.set_ylabel("Velocity (Body Lengths / second)")
+
+    vertical_line1 = ax.axvline(x=time[50], color='red', linestyle=':', visible=False)
+    vertical_line2 = ax.axvline(x=time[130], color='red', linestyle=':', visible=False)
+    vertical_line3 = ax.axvline(x=time[200], color='red', linestyle=':', visible=False)
+    vertical_line4 = ax.axvline(x=time[250], color='red', linestyle=':', visible=False)
+    vertical_line5 = ax.axvline(x=time[330], color='red', linestyle=':', visible=False)
+
+    # Initialization function: plot the background of each frame
+    def init():
+        line.set_ydata([np.nan] * len(time))
+        vertical_line1.set_visible(False)
+        vertical_line2.set_visible(False)
+        vertical_line3.set_visible(False)
+        vertical_line4.set_visible(False)
+        vertical_line5.set_visible(False)
+
+        return line, vertical_line1, vertical_line2, vertical_line3, vertical_line4, vertical_line5
+
+    # Animation function: update the line at each frame
+    def update(frame):
+        line.set_data(time[:frame], body_v[:frame])
+        if frame >= 50:
+            vertical_line1.set_visible(True)
+        if frame >= 130:
+            vertical_line2.set_visible(True)
+        if frame >= 200:
+            vertical_line3.set_visible(True)
+        if frame >= 250:
+            vertical_line4.set_visible(True)
+        if frame >= 330:
+            vertical_line5.set_visible(True)
         
-        return body_v
+        return line, vertical_line1, vertical_line2, vertical_line3, vertical_line4, vertical_line5
+
+    # Create the animation
+    ani = animation.FuncAnimation(fig, update, frames=len(time), init_func=init, blit=True, interval=3)
+
+    Writer = animation.FFMpegWriter(fps = 100, metadata=dict(artist = 'me'), bitrate = 1000)
+    ani.save("vert_wall_beetle_6_trial_2_cam-1.mp4", writer = Writer)
+    plt.show()
 
 
-body_v = stat_analysis(files)
-time = list(range(len(body_v)))
+#### Scatter animation ####
+def animation_scatter():
+    top = points[0]
+    middle = points[1]
+    bottom = points[2]
 
-fig, ax = plt.subplots()
-line, = ax.plot(time, body_v)
+    # Extract x and y values for each dataset
+    x_top, y_top = zip(*top)
+    x_middle, y_middle = zip(*middle)
+    x_bottom, y_bottom = zip(*bottom)
 
-# Set up plot limits
-ax.set_xlim(0, len(time))
-ax.set_ylim(np.min(body_v), np.max(body_v))
-ax.set_title("Body Velocity vs Time (Frames)")
-ax.set_xlabel("Frames (fps = 100)")
-ax.set_ylabel("Velocity (Body Lengths / second)")
+    # Create a figure and axis
+    fig, ax = plt.subplots()
+    line_top, = ax.plot([], [], color='purple', marker='o', linestyle='-', label='Top')
+    line_middle, = ax.plot([], [], 'go-', label='Middle')
+    line_bottom, = ax.plot([], [], 'ro-', label='Bottom')
 
-vertical_line = ax.axvline(x=time[125], color='red', linestyle=':', visible=False)
+    # Set axis limits
+    ax.set_xlim(min(x_top + x_middle + x_bottom) - 1, max(x_top + x_middle + x_bottom) + 1)
+    ax.set_ylim(min(y_top + y_middle + y_bottom) - 1, max(y_top + y_middle + y_bottom) + 1)
 
-# Initialization function: plot the background of each frame
-def init():
-    line.set_ydata([np.nan] * len(time))
-    vertical_line.set_visible(False)
-    return line, vertical_line
+    # Add legend
+    ax.legend()
 
-# Animation function: update the line at each frame
-def update(frame):
-    line.set_data(time[:frame], body_v[:frame])
-    if frame >= 125:
-        vertical_line.set_visible(True)
-    
-    return line, vertical_line
+    def init():
+        line_top.set_data([], [])
+        line_middle.set_data([], [])
+        line_bottom.set_data([], [])
+        return line_top, line_middle, line_bottom
 
-# Create the animation
-ani = animation.FuncAnimation(fig, update, frames=len(time), init_func=init, blit=True, interval=15)
+    def animate(i):
+        # Update the data for each line
+        line_top.set_data(x_top[i:i+1], y_top[i:i+1])
+        line_middle.set_data(x_middle[i:i+1], y_middle[i:i+1])
+        line_bottom.set_data(x_bottom[i:i+1], y_bottom[i:i+1])
+        return line_top, line_middle, line_bottom
 
-Writer = animation.FFMpegWriter(fps = 100, metadata=dict(artist = 'me'), bitrate = 1000)
-ani.save("Animation.mp4", writer = Writer)
-plt.show()
+    # Create an animation
+    ani = animation.FuncAnimation(fig, animate, init_func=init, frames=len(top), interval=1, blit=True)
+
+    # Display the animation
+    plt.show()
+"""
+
+
+
+
+
